cdcb7c18437018a102289a05874d5fc421c55935
Chelsea Dwarika <chelseakristina@live.com>
2022-10-22 17:39:15 +0000

    pair programming Kristy and Sam

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,32 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalJoin = verticalJoinner(letters).map(ls => ls.join(''));
+    const backwardsHorizontalJoin = letters.map(ls => ls.reverse().join(''));
+  
+    if (searchString(horizontalJoin, word)) return true;
+    if (searchString(backwardsHorizontalJoin, word)) return true;
+    if (searchString(verticalJoin, word)) return true;
+  
+    return false;
+  };
+  // searches through string
+  const searchString = (string,word) => {
+      for (let l of string) {
+          if (l.includes(word)) return true;
+      }
+  }
+  //joins array vertically
+  const verticalJoinner = (letters) => {
+    const newArray = [];
+    for (let outer in letters[0]) {
+      const tempArray = [];
+      for (let inner in letters) {
+        tempArray.push(letters[inner][outer]);
+      }
+      newArray.push(tempArray);
     }
-}
-
-module.exports = wordSearch
+      
+    return newArray;
+  };
+  
+  module.exports = wordSearch;
